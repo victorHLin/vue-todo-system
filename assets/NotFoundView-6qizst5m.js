8e327f459089347754328ca6f630e5b3448b67c7
victorHLin <lin1129@alumni.upenn.edu>
2024-08-27 21:45:18 -0500

    Updates

---
+++ assets/NotFoundView-6qizst5m.js
@@ -0,0 +1,1 @@
+import{u as t,g as s,c as n,o as u,a as o}from"./index-DMa2Zp7R.js";const c={id:"notFound"},_=o("h1",null,"Route Not Found",-1),a=o("div",null,"You will be redirected to login page",-1),d=[_,a],h={__name:"NotFoundView",setup(r){const e=t();return s(()=>{setTimeout(()=>{e.push("/")},3e3)}),(i,l)=>(u(),n("div",c,d))}};export{h as default};
