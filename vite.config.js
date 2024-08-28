9b43630fce2e4f12b6695e2292338c974eff466d
victorHLin <lin1129@alumni.upenn.edu>
2024-08-27 21:45:06 -0500

    Change base path

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VuePractice/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/vue-todo-system/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
